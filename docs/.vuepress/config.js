9a5c72d35a20da49086229068e6d68bd3e400566
shensiming <shensiming@bunanapp.com>
2023-07-27 22:29:16 +0800

    feat: 界面初始化完成

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,40 @@
 module.exports = {
   title: 'Hello VuePress',
   description: 'Just playing around',
-  base: '/vuepress-starter'
+  base: '/vuepress-starter',
+  themeConfig: {
+    nav: [
+      // NavbarItem
+      {
+        text: '首页',
+        link: '/',
+      },
+      {
+        text: '前端',
+        ariaLabel: 'Language Menu',
+        items: [
+          { text: 'Chinese', link: '/3d/' },
+          { text: 'vite', link: '/vite/' }
+        ]
+      },
+      // NavbarGroup
+      {
+        text: '关于',
+        link: '/about',
+        // children: ['/group/foo.md', '/group/bar.md'],
+      },
+      // 字符串 - 页面文件路径
+    ],
+    sidebar: [
+      {
+        title: 'Group 1',   // 必要的
+        path: '/about',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+        collapsable: false, // 可选的, 默认值是 true,
+        sidebarDepth: 1,    // 可选的, 默认值是 1
+        children: [
+          '/'
+        ]
+      },
+    ]
+  }
 }
